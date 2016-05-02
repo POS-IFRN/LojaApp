d7817b8cf45fee0f65454f40b6a4cedb9a64468a
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-05-02 16:23:38 -0300

    Criacao do cadastroFabricantes da concessionariaWPF

---
+++ LojaApp/CadastroFabricantes.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LojaApp
+{
+    /// <summary>
+    /// Interaction logic for CadastroFabricantes.xaml
+    /// </summary>
+    public partial class CadastroFabricantes : Window
+    {
+        public CadastroFabricantes()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelect_Click(object sender, RoutedEventArgs e)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            dataGrid.ItemsSource = (from f in dc.Fabricantes select f);
+        }
+    }
+}
